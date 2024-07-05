2fcf3a0e6f581b3b2695764a37c393ebf82aff97
Aurelien Brabant <aurelien@brabant.dev>
2024-07-05 23:11:04 +0200

    core: handle scoped keywords if flags are not allowed

--- tests/parse/main.cpp
+++ tests/parse/main.cpp
@@ -23,12 +23,13 @@ namespace Colors {
     }
 
 // globals for testing
-bool                          barrelRoll  = false;
-std::string                   flagsFound  = "";
-Hyprlang::CConfig*            pConfig     = nullptr;
-std::string                   currentPath = "";
+bool                            barrelRoll  = false;
+std::string                     flagsFound  = "";
+Hyprlang::CConfig*              pConfig     = nullptr;
+std::string                     currentPath = "";
+static std::vector<std::string> categoryKeywordActualValues;
 
-static Hyprlang::CParseResult handleDoABarrelRoll(const char* COMMAND, const char* VALUE) {
+static Hyprlang::CParseResult   handleDoABarrelRoll(const char* COMMAND, const char* VALUE) {
     if (std::string(VALUE) == "woohoo, some, params")
         barrelRoll = true;
 
@@ -42,6 +43,12 @@ static Hyprlang::CParseResult handleFlagsTest(const char* COMMAND, const char* V
 
     Hyprlang::CParseResult result;
     return result;
+}
+
+static Hyprlang::CParseResult handleCategoryKeyword(const char* COMMAND, const char* VALUE) {
+    categoryKeywordActualValues.push_back(VALUE);
+
+    return Hyprlang::CParseResult();
 }
 
 static Hyprlang::CParseResult handleSource(const char* COMMAND, const char* VALUE) {
@@ -88,12 +95,14 @@ int main(int argc, char** argv, char** envp) {
         config.addConfigValue("testStringColon", "");
         config.addConfigValue("testEnv", "");
         config.addConfigValue("testVar", (Hyprlang::INT)0);
+        config.addConfigValue("categoryKeyword", (Hyprlang::STRING) "");
         config.addConfigValue("testStringQuotes", "");
         config.addConfigValue("testStringRecursive", "");
         config.addConfigValue("testCategory:testValueInt", (Hyprlang::INT)0);
         config.addConfigValue("testCategory:testValueHex", (Hyprlang::INT)0xA);
         config.addConfigValue("testCategory:nested1:testValueNest", (Hyprlang::INT)0);
         config.addConfigValue("testCategory:nested1:nested2:testValueNest", (Hyprlang::INT)0);
+        config.addConfigValue("testCategory:nested1:categoryKeyword", (Hyprlang::STRING) "");
         config.addConfigValue("testDefault", (Hyprlang::INT)123);
         config.addConfigValue("testCategory:testColor1", (Hyprlang::INT)0);
         config.addConfigValue("testCategory:testColor2", (Hyprlang::INT)0);
@@ -107,6 +116,7 @@ int main(int argc, char** argv, char** envp) {
         config.registerHandler(&handleDoABarrelRoll, "doABarrelRoll", {false});
         config.registerHandler(&handleFlagsTest, "flags", {true});
         config.registerHandler(&handleSource, "source", {false});
+        config.registerHandler(&handleCategoryKeyword, "testCategory:categoryKeyword", {false});
 
         config.addSpecialCategory("special", {"key"});
         config.addSpecialConfigValue("special", "value", (Hyprlang::INT)0);
@@ -161,6 +171,10 @@ int main(int argc, char** argv, char** envp) {
         std::cout << " → Testing handlers\n";
         EXPECT(barrelRoll, true);
         EXPECT(flagsFound, std::string{"abc"});
+
+        EXPECT(categoryKeywordActualValues[0], "we are having fun");
+        EXPECT(categoryKeywordActualValues[1], "so much fun");
+        EXPECT(categoryKeywordActualValues[2], "im the fun one at parties");
 
         // test dynamic
         std::cout << " → Testing dynamic\n";
